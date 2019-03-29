d8c18a9ec5d74f9d40dd3b8799cd249e05c661ae
robyn <robyn3choi@gmail.com>
2019-03-28 18:02:21 -0700

    added forget location button, prevented from rendering landign page if location is stored

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Footer.css'
 
-const Footer = (props) => {
-  return (
-    <div className={`footer ${props.isMainPage ? 'footer_main-page' : 'footer_landing-page'}`}>
-      <div className='footer__buttons'>
-        <a href='https://github.com/robyn3choi/simple-weather/blob/master/TermsOfUseAndPrivacyPolicy.md' className='footer__link' target='_blank' rel='noopener noreferrer'>
-          Terms of Use and Privacy Policy
-        </a>
+class Footer extends Component {
+  state = { isLocationStored: localStorage.getItem('placeName') && true }
+
+  removeStoredLocation = () =>{
+    localStorage.removeItem('placeName');
+    this.setState({isLocationStored: false});
+  }
+
+  render() {
+    const buttonText = this.state.isLocationStored ? 'Forget my location' : 'Your location is no longer stored';
+    return (
+      <div className={`footer ${this.props.isMainPage ? 'footer_main-page' : 'footer_landing-page'}`}>
+        <div className='footer__line'>
+          {this.props.isMainPage ? (
+            <button className='footer__button' disabled={!this.state.isLocationStored} onClick={this.removeStoredLocation}>
+              {buttonText}
+            </button>)
+            : null}
+        </div>
+        <div className='footer__line'>
+          <a href='https://github.com/robyn3choi/simple-weather/blob/master/TermsOfUseAndPrivacyPolicy.md' className='footer__link' target='_blank' rel='noopener noreferrer'>
+            Terms of Use and Privacy Policy
+          </a>
+        </div>
+        <div className='footer__line'>
+          Icons from <a href='https://github.com/zagortenay333/Tempestacons'>Tempestacons</a> and formatted by <a href='https://github.com/rickellis/SVG-Weather-Icons'>rickellis</a>
+        </div>
       </div>
-      <div className='icons-attribution footer__attribution'>
-        Icons from <a href='https://github.com/zagortenay333/Tempestacons'>Tempestacons</a> and formatted by <a href='https://github.com/rickellis/SVG-Weather-Icons'>rickellis</a>
-      </div>
-      <div className='dark-sky-attribution footer__attribution'>
-        Powered by <a href='https://darksky.net/poweredby/'>Dark Sky</a>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Footer;
