5a3c25563876fc05d7aac346c0c638bcd0856674
Robyn Choi <robyn3choi@gmail.com>
2019-04-08 19:35:17 -0700

    made description text resize if too big

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -10,16 +10,16 @@ class Footer extends Component {
   }
 
   render() {
-    const buttonText = this.state.isLocationStored ? 'Forget my location' : 'Your location is no longer stored';
+    //const buttonText = this.state.isLocationStored ? 'Forget my location' : 'Your location is no longer stored';
     return (
       <div className={`footer ${this.props.isMainPage ? 'footer_main-page' : 'footer_landing-page'}`}>
-        <div className='footer__line'>
+        {/* <div className='footer__line'>
           {this.props.isMainPage ? (
             <button className='footer__button' disabled={!this.state.isLocationStored} onClick={this.removeStoredLocation}>
               {buttonText}
             </button>)
             : null}
-        </div>
+        </div> */}
         <div className='footer__line'>
           <a href='https://github.com/robyn3choi/simple-weather/blob/master/TermsOfUseAndPrivacyPolicy.md' className='footer__link' target='_blank' rel='noopener noreferrer'>
             Terms of Use and Privacy Policy
--- src/Forecast/ForecastDay.js
+++ src/Forecast/ForecastDay.js
@@ -1,37 +1,93 @@
-import React from 'react';
+import React, {Component} from 'react';
 import FadeIn from './../FadeIn/FadeIn';
 import * as Constants from './../Constants';
 import './ForecastDay.css';
 
-const ForecastDay = (props) => {
-  return (
-    <FadeIn counter={props.entries} timeout={160 + 80 * props.index}>
-      <div className={`forecast-day ${props.index % 2 === 0 ? 'forecast-day_light' : ''}`}>
-        <div className='forecast-day__weekday-date'>
-          <div className='forecast-day__weekday'>{props.index === 0 ? 'Today' : Constants.WEEKDAY_NAMES[props.data.weekday]}</div>
-          <div className='forecast-day__date'>{props.data.month + 1}/{props.data.date}</div>
-        </div>
-        <img className='forecast-day__main-icon' alt='weather-icon' src={'/icons/' + props.data.icon + '.svg'} />
-        <div className='forecast-day__description'>{props.data.description}</div>
-        <div className='forecast-day__icons-with-text'>
-          <div className='forecast-day__icon-with-text'>
-            <img className='forecast-day__icon' alt='temperature' src={'/icons/temperature.svg'} />
-            <div className='forecast-day__icon-text'>{props.data.tempHigh}&deg;/{props.data.tempLow}&deg;</div>
+class ForecastDay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { descHasSmallerFont: false }
+    this.descRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.descHeight = this.descRef.current.scrollHeight;
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+  }
+
+  handleResize = () => {
+    this.setState({descHasSmallerFont: this.descHeight > this.descRef.current.clientHeight});
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.data.description !== this.props.data.description) {
+      this.descHeight = this.descRef.current.scrollHeight;
+      this.handleResize();
+    }
+  }
+
+  render() {
+    return (
+      <FadeIn counter={this.props.entries} timeout={160 + 80 * this.props.index}>
+        <div className={`forecast-day ${this.props.index % 2 === 0 ? 'forecast-day_light' : ''}`}>
+          <div className='forecast-day__weekday-date'>
+            <div className='forecast-day__weekday'>{this.props.index === 0 ? 'Today' : Constants.WEEKDAY_NAMES[this.props.data.weekday]}</div>
+            <div className='forecast-day__date'>{this.props.data.month + 1}/{this.props.data.date}</div>
           </div>
-          <div className='forecast-day__icon-with-text'>
-            <img className='forecast-day__icon' alt='wind-speed' src={'/icons/wind.svg'} />
-            <div className='forecast-day__text'>{props.data.wind}
-              <span className='forecast-day__wind-speed-units'>{` ${Constants.WINDSPEED_UNITS[props.units]}`}</span>
+          <img className='forecast-day__main-icon' alt='weather-icon' src={'/icons/' + this.props.data.icon + '.svg'} />
+          <div className={`forecast-day__description ${this.state.descHasSmallerFont ? 'forecast-day__description_small' : ''}`} ref={this.descRef}>{this.props.data.description}</div>
+          <div className='forecast-day__icons-with-text'>
+            <div className='forecast-day__icon-with-text'>
+              <img className='forecast-day__icon' alt='temperature' src={'/icons/temperature.svg'} />
+              <div className='forecast-day__icon-text'>{this.props.data.tempHigh}&deg;/{this.props.data.tempLow}&deg;</div>
+            </div>
+            <div className='forecast-day__icon-with-text'>
+              <img className='forecast-day__icon' alt='wind-speed' src={'/icons/wind.svg'} />
+              <div className='forecast-day__text'>{this.props.data.wind}
+                <span className='forecast-day__wind-speed-units'>{` ${Constants.WINDSPEED_UNITS[this.props.units]}`}</span>
+              </div>
+            </div>
+            <div className='forecast-day__icon-with-text'>
+              <img className='forecast-day__icon' alt='probability-of-precipitation' src={'/icons/umbrella.svg'} />
+              <div className='forecast-day__text'>{this.props.data.precipChance}%</div>
             </div>
           </div>
-          <div className='forecast-day__icon-with-text'>
-            <img className='forecast-day__icon' alt='probability-of-precipitation' src={'/icons/umbrella.svg'} />
-            <div className='forecast-day__text'>{props.data.precipChance}%</div>
-          </div>
         </div>
-      </div>
-    </FadeIn>
-  );
+      </FadeIn>
+    );
+  }
 }
+
+// const ForecastDay = (this.props) => {
+//   return (
+//     <FadeIn counter={this.props.entries} timeout={160 + 80 * this.props.index}>
+//       <div className={`forecast-day ${this.props.index % 2 === 0 ? 'forecast-day_light' : ''}`}>
+//         <div className='forecast-day__weekday-date'>
+//           <div className='forecast-day__weekday'>{this.props.index === 0 ? 'Today' : Constants.WEEKDAY_NAMES[this.props.data.weekday]}</div>
+//           <div className='forecast-day__date'>{this.props.data.month + 1}/{this.props.data.date}</div>
+//         </div>
+//         <img className='forecast-day__main-icon' alt='weather-icon' src={'/icons/' + this.props.data.icon + '.svg'} />
+//         <div className='forecast-day__description'>{this.props.data.description}</div>
+//         <div className='forecast-day__icons-with-text'>
+//           <div className='forecast-day__icon-with-text'>
+//             <img className='forecast-day__icon' alt='temperature' src={'/icons/temperature.svg'} />
+//             <div className='forecast-day__icon-text'>{this.props.data.tempHigh}&deg;/{this.props.data.tempLow}&deg;</div>
+//           </div>
+//           <div className='forecast-day__icon-with-text'>
+//             <img className='forecast-day__icon' alt='wind-speed' src={'/icons/wind.svg'} />
+//             <div className='forecast-day__text'>{this.props.data.wind}
+//               <span className='forecast-day__wind-speed-units'>{` ${Constants.WINDSPEED_UNITS[this.props.units]}`}</span>
+//             </div>
+//           </div>
+//           <div className='forecast-day__icon-with-text'>
+//             <img className='forecast-day__icon' alt='probability-of-precipitation' src={'/icons/umbrella.svg'} />
+//             <div className='forecast-day__text'>{this.props.data.precipChance}%</div>
+//           </div>
+//         </div>
+//       </div>
+//     </FadeIn>
+//   );
+// }
 
 export default ForecastDay;
