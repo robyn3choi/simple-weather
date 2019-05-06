85cf18742d0cc1221bc30f9cbbbb8ae354562aa4
robyn <robyn3choi@gmail.com>
2019-05-05 22:09:49 -0700

    made detect location error msg

--- src/DetectMyLocation/DetectMyLocation.js
+++ src/DetectMyLocation/DetectMyLocation.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './DetectMyLocation.css';
 
-const detectLocation = (props) => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(pos => {
-      const coords = { lat: pos.coords.latitude, long: pos.coords.longitude };
-      props.setWeatherDataFromPosition(coords)
-    }, err => console.log(err));
-  }
-}
+const DetectMyLocation = props => {
+  const [isErrorMsgDisplayed, setIsErrorMsgDisplayed] = useState(false);
 
-const DetectMyLocation = (props) => {
+  const detectLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        pos => {
+          const coords = { lat: pos.coords.latitude, long: pos.coords.longitude };
+          props.setWeatherDataFromPosition(coords);
+        },
+        err => {
+          setIsErrorMsgDisplayed(true);
+        }
+      );
+    }
+  };
+
   return (
-    <div className='location-button-container'>
-      <button className={`location-button`}
-        onClick={() => detectLocation(props)}>
-        Detect my location
-          </button>
+    <div>
+      <div className="location-button-container">
+        <button className={`location-button`} onClick={() => detectLocation()}>
+          Detect my location
+        </button>
+      </div>
+      <div className={`location-error ${isErrorMsgDisplayed? 'location-error_displayed' : ''}`}>
+          <div className="location-error__text">
+            There was a problem detecting your location. If you are on mobile, make sure any screen overlays
+            are turned off.
+          </div>
+          <button className="location-error__exit-btn" onClick={() => setIsErrorMsgDisplayed(false)}>x</button>
+        </div>
     </div>
   );
-}
+};
 
 export default DetectMyLocation;
